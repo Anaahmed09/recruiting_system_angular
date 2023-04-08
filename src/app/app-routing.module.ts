fdf333f8bb81196c3c29285e3a9a658f3196943d
akosary <a7mdkosary@gmail.com>
2023-04-09 00:43:25 +0200

    fix conflict2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,30 +2,29 @@ import { NotFoundComponent } from './components/shared/not-found/not-found.compo
 import { LoginComponent } from './components/shared/login/login.component';
 import { ResultComponent } from './components/result/result.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { NgModule,Component } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { JobComponent } from './components/job/job.component';
 import { ApplyjobComponent } from './components/applyjob/applyjob.component';
 import { RouterModule, Routes } from '@angular/router';
-import { WelcomeComponent } from './components/user/welcome/welcome.component';
 import { AppComponent } from './app.component';
 
-const routes: Routes = [{ path: 'welcome', component: WelcomeComponent }];
 import { RegisterFormComponent } from './components/user/register-form/register-form.component';
 import { AllJobsComponent } from './components/admin/all-jobs/all-jobs.component';
-import { WelcomeComponent } from './components/user/welcome/welcome.component';
 import { ShowAllCandidatesComponent } from './components/show-all-candidates/show-all-candidates.component';
 import { CreateQuestionComponent } from './components/create-question/create-question.component';
 
-
 const routes: Routes = [
-  {path:'welcome', component :ApplyjobComponent },
-  {path:'userjob', component : JobComponent},
-  {path:'apply/:id', component :ApplyjobComponent },
-  { path: 'profile', component: ProfileComponent},
-  { path:'result', component: ResultComponent, }
+  { path: 'welcome', component: ApplyjobComponent },
+  { path: 'userjob', component: JobComponent },
+  { path: 'apply/:id', component: ApplyjobComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'result', component: ResultComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', component: NotFoundComponent },
-  { path: 'createQuestion/:question_id/:job_id', component: CreateQuestionComponent },
+  {
+    path: 'createQuestion/:question_id/:job_id',
+    component: CreateQuestionComponent,
+  },
   { path: 'showAllCandidates', component: ShowAllCandidatesComponent },
   {
     path: 'signup',
@@ -34,10 +33,6 @@ const routes: Routes = [
   {
     path: 'all-jobs',
     component: AllJobsComponent,
-  },
-  {
-    path: 'addJob',
-    component: WelcomeComponent,
   },
   {
     path: '',
